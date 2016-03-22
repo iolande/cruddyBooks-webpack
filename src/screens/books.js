a1a0a56fe3a32a055b13f8dc216a3daac628f2b5
io <iolande@users.noreply.github.com>
2016-03-22 21:08:00 +0000

    Rationalise book screen

--- src/screens/books.js
+++ src/screens/books.js
@@ -1,13 +1,33 @@
 import {inject} from 'aurelia-framework';
 import {BookCollection} from '../collections/bookCollection';
+import {BookService} from '../services/bookService';
 
-@inject(BookCollection)
+@inject(BookCollection, BookService)
 export class Books {
-  constructor(bookCollection) {
+  searchTerm = '';
+  selectedBook = {
+    title: '',
+    author: '',
+    genre: '',
+    read: false
+  };
+
+  constructor(bookCollection, bookService) {
     this.bookCollection = bookCollection;
+    this.bookService = bookService;
   }
 
   activate() {
-    return this.bookCollection.getAllBooks();
+    return this.bookCollection.getAllBooks()
+      .then(() => this.books = this.bookCollection.books);
+  }
+
+  doSearchByGenre() {
+    return this.bookService.getBooksByGenre(this.searchTerm)
+      .then(response => this.bookList = response);
+  }
+
+  selectBook(book) {
+    this.selectedBook = book;
   }
 }
--- src/screens/panels/bookInfo.js
+++
@@ -1,9 +0,0 @@
-import {inject} from 'aurelia-framework';
-import {BookCollection} from '../../collections/bookCollection';
-
-@inject(BookCollection)
-export class BookInfo {
-  constructor(bookCollection) {
-    this.books = bookCollection.books;
-  }
-}
